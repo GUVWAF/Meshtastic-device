ddbe1079f946e8f1e601ba970172a37ccfa204ec
GUVWAF <thijs@havinga.eu>
2023-12-06 21:59:17 +0100

    Replace `pico` for Challenger RP2040 LoRa

--- variants/rpipico/variant.h
+++ variants/rpipico/variant.h
@@ -14,16 +14,11 @@
 // txd = 8
 // rxd = 9
 
-#define EXT_NOTIFY_OUT 22
-#define BUTTON_PIN 17
-
 #define LED_PIN PIN_LED
 
 #define BATTERY_PIN 26
 // ratio of voltage divider = 3.0 (R17=200k, R18=100k)
 #define ADC_MULTIPLIER 3.1 // 3.0 + a bit for being optimistic
-
-#define USE_SX1262
 
 #undef LORA_SCK
 #undef LORA_MISO
@@ -33,19 +28,10 @@
 #define LORA_SCK 10
 #define LORA_MISO 12
 #define LORA_MOSI 11
-#define LORA_CS 3
 
-#define LORA_DIO0 RADIOLIB_NC
-#define LORA_RESET 15
-#define LORA_DIO1 20
-#define LORA_DIO2 2
-#define LORA_DIO3 RADIOLIB_NC
-
-#ifdef USE_SX1262
-#define SX126X_CS LORA_CS
-#define SX126X_DIO1 LORA_DIO1
-#define SX126X_BUSY LORA_DIO2
-#define SX126X_RESET LORA_RESET
-#define SX126X_DIO2_AS_RF_SWITCH
-#define SX126X_DIO3_TCXO_VOLTAGE 1.8
-#endif
+#define USE_RF95
+#define LORA_DIO0 14
+#define LORA_DIO1 15
+#define LORA_DIO2 18
+#define LORA_CS 9
+#define LORA_RESET 13
